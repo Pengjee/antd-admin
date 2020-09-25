0e6b1e32d4be0dcf3a307cd06e3e273e803b2819
pengji <pengji@qipeng.com>
2020-09-25 13:57:06 +0800

    基础框架

--- router/index.js
+++ router/index.js
@@ -41,32 +41,7 @@ export const router = [ {
 			routes: [
 				{
 					path: '/',
-					redirect: '/dashboard/analysis',
-				},
-				{
-					path: '/dashboard',
-					name: 'dashboard',
-					icon: 'dashboard',
-					routes: [
-						{
-							name: 'analysis',
-							icon: 'smile',
-							path: '/dashboard/analysis',
-							component: './dashboard/analysis',
-						},
-						{
-							name: 'monitor',
-							icon: 'smile',
-							path: '/dashboard/monitor',
-							component: './dashboard/monitor',
-						},
-						{
-							name: 'workplace',
-							icon: 'smile',
-							path: '/dashboard/workplace',
-							component: './dashboard/workplace',
-						},
-					],
+					redirect: '/form/basic-form',
 				},
 				{
 					path: '/form',
@@ -99,52 +74,21 @@ export const router = [ {
 					name: 'list',
 					routes: [
 						{
-							path: '/list/search',
-							name: 'search-list',
-							component: './list/search',
-							routes: [
-								{
-									path: '/list/search',
-									redirect: '/list/search/articles',
-								},
-								{
-									name: 'articles',
-									icon: 'smile',
-									path: '/list/search/articles',
-									component: './list/search/articles',
-								},
-								{
-									name: 'projects',
-									icon: 'smile',
-									path: '/list/search/projects',
-									component: './list/search/projects',
-								},
-								{
-									name: 'applications',
-									icon: 'smile',
-									path: '/list/search/applications',
-									component: './list/search/applications',
-								},
-							],
-						},
-						{
 							name: 'table-list',
 							icon: 'smile',
 							path: '/list/table-list',
 							component: './list/table-list',
-						},
-						{
+						}, {
 							name: 'basic-list',
 							icon: 'smile',
 							path: '/list/basic-list',
 							component: './list/basic-list',
-						},
-						{
+						}, {
 							name: 'card-list',
 							icon: 'smile',
 							path: '/list/card-list',
 							component: './list/card-list',
-						},
+						}
 					],
 				},
 				{
@@ -163,25 +107,6 @@ export const router = [ {
 							icon: 'smile',
 							path: '/profile/advanced',
 							component: './profile/advanced',
-						},
-					],
-				},
-				{
-					name: 'result',
-					icon: 'CheckCircleOutlined',
-					path: '/result',
-					routes: [
-						{
-							name: 'success',
-							icon: 'smile',
-							path: '/result/success',
-							component: './result/success',
-						},
-						{
-							name: 'fail',
-							icon: 'smile',
-							path: '/result/fail',
-							component: './result/fail',
 						},
 					],
 				},
@@ -230,40 +155,15 @@ export const router = [ {
 					],
 				},
 				{
-					name: 'editor',
-					icon: 'highlight',
-					path: '/editor',
-					routes: [
-						{
-							name: 'flow',
-							icon: 'smile',
-							path: '/editor/flow',
-							component: './editor/flow',
-						},
-						{
-							name: 'mind',
-							icon: 'smile',
-							path: '/editor/mind',
-							component: './editor/mind',
-						},
-						{
-							name: 'koni',
-							icon: 'smile',
-							path: '/editor/koni',
-							component: './editor/koni',
-						},
-					],
-				},
-				{
 					name: '自定义页面',
 					icon: 'smile',
-					path: '/custom',
-					routes: [{
+					path: '/domestic',
+					routes: [ {
 						name: '自定义',
 						icon: 'smile',
-						path: '/custom/page',
-						component: './domestic/template',
-					}]
+						path: '/domestic/template',
+						component: './domestic/TemplateRecords',
+					} ]
 				},
 				{
 					component: '404',
